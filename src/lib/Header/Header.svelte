e8e571e4fd3cc046108dddf0ecf7f7c2b4d21b8b
Mukesh Kr. Chaudhary <mukezhz@gmail.com>
2023-06-10 11:34:05 +0545

    ✨ feat: adds header for landing page

---
+++ src/lib/Header/Header.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+	let hidden = true;
+	const toggleHiddenMenu = () => (hidden = !hidden);
+</script>
+
+<header>
+	<nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+		<div class="flex lg:flex-1">
+			<a href="#" class="-m-1.5 p-1.5">
+				<span class="sr-only">Your Company</span>
+				<img
+					class="h-8 w-auto"
+					src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+					alt=""
+				/>
+			</a>
+		</div>
+		<div class="flex lg:hidden">
+			<button
+				type="button"
+				class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
+				class:hidden={!hidden}
+				on:click={toggleHiddenMenu}
+			>
+				<span class="sr-only">Open main menu</span>
+				<svg
+					class="h-6 w-6"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					aria-hidden="true"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+					/>
+				</svg>
+			</button>
+		</div>
+		<div class="hidden lg:flex lg:gap-x-12">
+			<a href="#" class="text-sm font-semibold leading-6">Product</a>
+			<a href="#" class="text-sm font-semibold leading-6">Features</a>
+			<a href="#" class="text-sm font-semibold leading-6">Marketplace</a>
+			<a href="#" class="text-sm font-semibold leading-6">Company</a>
+		</div>
+		<div class="hidden lg:flex lg:flex-1 lg:justify-end">
+			<a href="#" class="text-sm font-semibold leading-6"
+				>Log in <span aria-hidden="true">&rarr;</span></a
+			>
+		</div>
+		<div class="hidden lg:flex ml-3">
+			<LightSwitch />
+		</div>
+	</nav>
+	<!-- Mobile menu, show/hide based on menu open state. -->
+	<div class="lg:hidden" class:hidden role="dialog" aria-modal="true">
+		<!-- Background backdrop, show/hide based on slide-over state. -->
+		<div class="fixed inset-0 z-10" />
+		<div
+			class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+		>
+			<div class="flex items-center justify-between">
+				<button type="button" class="-m-2.5 rounded-md p-2.5" on:click={toggleHiddenMenu}>
+					<span class="sr-only">Close menu</span>
+					<svg
+						class="h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						aria-hidden="true"
+					>
+						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+					</svg>
+				</button>
+			</div>
+			<div class="mt-6 flow-root">
+				<div class="-my-6 divide-y divide-gray-500/10">
+					<div class="space-y-2 py-6">
+						<a
+							href="#"
+							class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50"
+							>Product</a
+						>
+						<a
+							href="#"
+							class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50"
+							>Features</a
+						>
+						<a
+							href="#"
+							class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50"
+							>Marketplace</a
+						>
+						<a
+							href="#"
+							class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50"
+							>Company</a
+						>
+					</div>
+					<div class="py-6">
+						<a
+							href="#"
+							class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 hover:bg-gray-50"
+							>Login</a
+						>
+					</div>
+					<div class="py-6">
+						<LightSwitch />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
