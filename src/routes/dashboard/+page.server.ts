99e92716819dba9ba7a2bda28fabccaf299145cd
Mukesh Kr. Chaudhary <mukezhz@gmail.com>
2023-06-11 21:09:38 +0545

    fix: removes unnecessary load from page.svelte.ts

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -2,10 +2,10 @@ export const ssr = false;
 export const csr = true;
 export const prerender = true;
 
-import type { PageServerLoad } from './$types';
+// import type { PageServerLoad } from './$types';
 
-export const load = (async ({ fetch }) => {
-	console.log('server load function called');
-	const title = 'List of available posts';
-	return { post: { title } };
-}) satisfies PageServerLoad;
+// export const load = (async ({ fetch }) => {
+// 	console.log('server load function called');
+// 	const title = 'List of available posts';
+// 	return { post: { title } };
+// }) satisfies PageServerLoad;
