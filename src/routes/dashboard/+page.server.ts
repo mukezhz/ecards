6e2f0b679dea92fdf51b2e332d39230bec654e51
Mukesh Kr. Chaudhary <mukezhz@gmail.com>
2023-06-11 21:05:36 +0545

    refactor: removes unnecessary console.log and api fetch

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -7,7 +7,5 @@ import type { PageServerLoad } from './$types';
 export const load = (async ({ fetch }) => {
 	console.log('server load function called');
 	const title = 'List of available posts';
-	const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-	const posts = await response.json();
-	return { post: { title, posts } };
+	return { post: { title } };
 }) satisfies PageServerLoad;
