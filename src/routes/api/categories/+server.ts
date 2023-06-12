a4839c8996ff18cf19c432ade3a4ffcf17e1c667
Mukesh Kr. Chaudhary <mukezhz@gmail.com>
2023-06-12 08:23:31 +0545

    feat: implements feature to add category

--- src/routes/api/categories/+server.ts
+++ src/routes/api/categories/+server.ts
@@ -1,5 +1,8 @@
 import { databases } from '$lib/server/db';
 import { DB_CONSTANT } from '$lib/server/constant';
+import type { RequestEvent } from './$types';
+import { ID } from 'node-appwrite';
+import { json } from '@sveltejs/kit';
 
 export async function GET() {
 	const categories = await databases.listDocuments(DB_CONSTANT.DATABASE, DB_CONSTANT.CATEGORIES);
@@ -14,4 +17,37 @@ export async function GET() {
 			}
 		}
 	);
+}
+
+export async function POST({ request }) {
+	const body = await request.json();
+	const { name, priority, published, trending, image, usedCount, createdBy } = body;
+
+	const id = ID.unique();
+	const now = new Date();
+	const categories = await databases.createDocument(
+		DB_CONSTANT.DATABASE,
+		DB_CONSTANT.CATEGORIES,
+		ID.unique(),
+		{
+			name,
+			priority,
+			published,
+			trending,
+			image,
+			used_count: usedCount,
+			created_at: now,
+			updated_at: now,
+			owner_metadata: JSON.stringify({
+				id,
+				name: createdBy
+			})
+		}
+	);
+	return json({
+		message: 'success!!!',
+		data: {
+			id: categories.$id
+		}
+	});
 }
